854fa4c377283b6c594cc3ba3eda378cb353d6fb
carloosbaquero <carlos.baqueville@gmail.com>
2025-03-20 17:22:04 +0100

    tests commented

--- src/app/components/security/register/register.component.spec.ts
+++ src/app/components/security/register/register.component.spec.ts
@@ -1,17 +1,32 @@
 // import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 // import { RegisterComponent } from './register.component';
+// import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
+// import { HttpClientModule } from '@angular/common/http';
+// import { provideAuth } from '@angular/fire/auth';
+// import { getAuth } from 'firebase/auth';
+// import { environment } from 'src/environments/environment';
+// import { AuthGuard } from 'src/app/guards/auth.guard';
+// import { AuthService } from 'src/app/services/auth.service';
 
 // describe('RegisterComponent', () => {
 //   let component: RegisterComponent;
 //   let fixture: ComponentFixture<RegisterComponent>;
+//   let service: AuthService;
 
 //   beforeEach(async () => {
 //     await TestBed.configureTestingModule({
-//       declarations: [ RegisterComponent ]
+//       declarations: [ RegisterComponent ],
+//       imports: [
+//                 HttpClientModule,
+//                 provideFirebaseApp(() => initializeApp(environment.firebaseConfig)),
+//                 provideAuth(() => getAuth())
+//               ],
+//               providers: [AuthGuard]
 //     })
 //     .compileComponents();
 
+//     service = TestBed.inject(AuthService);
 //     fixture = TestBed.createComponent(RegisterComponent);
 //     component = fixture.componentInstance;
 //     fixture.detectChanges();
--- src/app/services/auth.service.spec.ts
+++ src/app/services/auth.service.spec.ts
@@ -1,24 +1,65 @@
-import { BehaviorSubject } from "rxjs";
-import { AuthService } from "./auth.service";
+import { TestBed } from '@angular/core/testing';
+import { AuthService } from './auth.service';
+import {provideAuth, getAuth} from '@angular/fire/auth';
+import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
+import { environment } from 'src/environments/environment';
+import { AuthGuard } from '../guards/auth.guard';
+import { HttpClientModule } from '@angular/common/http';
+import { Actor } from '../models/actor.model';
 
 describe('AuthService', () => {
-    let service: AuthService;
-    beforeEach(() => {
-      service = new AuthService();
-    });
-    it('#getValue should return real value', () => {
-      expect(service.getValue()).toBe('real value');
-    });
-    it('#getObservableValue should return value from observable', (done: DoneFn) => {
-      service.getObservableValue().subscribe((value) => {
-        expect(value).toBe('observable value');
-        done();
-      });
-    });
-    it('#getPromiseValue should return value from a promise', (done: DoneFn) => {
-      service.getPromiseValue().then((value) => {
-        expect(value).toBe('promise value');
-        done();
-      });
-    });
+  let service: AuthService;
+  let auth: any;
+
+  beforeEach(async () => {
+    TestBed.configureTestingModule({
+      imports: [
+          HttpClientModule,
+          provideFirebaseApp(() => initializeApp(environment.firebaseConfig)),
+          provideAuth(() => getAuth())
+        ],
+        providers: [AuthGuard]
+    })
+    service = TestBed.inject(AuthService);
+    auth = getAuth();
   });
+
+  it('should be created', () => {
+    expect(service).toBeTruthy();
+  });
+
+  it('should return roles', () => {
+    expect(service.getRoles()).toEqual(['CLERK', 'ADMINISTRATOR', 'CONSUMER']);
+  });
+
+  // it('should sign up, log in, and log out', async () => {
+  //   // Create an instance of Actor
+  //   const actor = new Actor();
+  //   actor.name = 'Test Name';
+  //   actor.surname = 'Test Surname';
+  //   actor.phone = '123456789';
+  //   actor.role = 'ADMINISTRATOR';
+  //   actor.email = 'newuser15@example.com';
+  //   actor.password = 'password123';
+
+  //   await service.signUp(actor);
+
+  //   const responseLogOut = await service.logout();
+  //   console.log(responseLogOut)
+
+  //   const responseLogin = await service.login(actor.email, actor.password);
+  //   expect(responseLogin).toBeDefined();
+  //   const user = auth.currentUser;
+  //   console.log('User: ', user);
+  //   expect(user).toBeDefined();
+
+    // // Delete the user
+    // if (user) {
+    //   await user.delete();
+    
+    //   // Verifica si el usuario ha sido eliminado correctamente
+    //   const refreshedUser = auth.currentUser; 
+    //   expect(refreshedUser).toBeNull(); // Ahora sí debe ser null
+    // }
+  // });
+});
