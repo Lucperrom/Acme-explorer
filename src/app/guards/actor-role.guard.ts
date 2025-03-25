ae52a62930187085e40d0fc5ca90f3e1da85c2ec
carloosbaquero <carlos.baqueville@gmail.com>
2025-03-25 20:51:48 +0100

    dennied-access fixed

--- src/app/guards/actor-role.guard.ts
+++ src/app/guards/actor-role.guard.ts
@@ -28,7 +28,7 @@ export class ActorRoleGuard implements CanActivate {
             let result = false;
             if(currentActor){
                 const activeRole = new RegExp(currentActor.role.toString(), 'i');
-                if(expectedRole.search(activeRole) !== 1){
+                if(expectedRole.search(activeRole) !== -1){
                     result = true;
                 } else {
                     this.router.navigate(['denied-access'],
