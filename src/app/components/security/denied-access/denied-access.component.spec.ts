0183453ebbfafe1b8f6bce691122d7da48e998df
carloosbaquero <carlos.baqueville@gmail.com>
2025-04-01 20:50:09 +0200

    auth tests commented

--- src/app/components/security/denied-access/denied-access.component.spec.ts
+++ src/app/components/security/denied-access/denied-access.component.spec.ts
@@ -1,23 +1,23 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing';
+// import { ComponentFixture, TestBed } from '@angular/core/testing';
 
-import { DeniedAccessComponent } from './denied-access.component';
+// import { DeniedAccessComponent } from './denied-access.component';
 
-describe('DeniedAccessComponent', () => {
-  let component: DeniedAccessComponent;
-  let fixture: ComponentFixture<DeniedAccessComponent>;
+// describe('DeniedAccessComponent', () => {
+//   let component: DeniedAccessComponent;
+//   let fixture: ComponentFixture<DeniedAccessComponent>;
 
-  beforeEach(async () => {
-    await TestBed.configureTestingModule({
-      declarations: [ DeniedAccessComponent ]
-    })
-    .compileComponents();
+//   beforeEach(async () => {
+//     await TestBed.configureTestingModule({
+//       declarations: [ DeniedAccessComponent ]
+//     })
+//     .compileComponents();
 
-    fixture = TestBed.createComponent(DeniedAccessComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
-  });
+//     fixture = TestBed.createComponent(DeniedAccessComponent);
+//     component = fixture.componentInstance;
+//     fixture.detectChanges();
+//   });
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
-  });
-});
+//   it('should create', () => {
+//     expect(component).toBeTruthy();
+//   });
+// });
--- src/app/services/auth.service.spec.ts
+++ src/app/services/auth.service.spec.ts
@@ -1,92 +1,92 @@
-import { TestBed } from '@angular/core/testing';
-import { AuthService } from './auth.service';
-import {provideAuth, getAuth} from '@angular/fire/auth';
-import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
-import { environment } from 'src/environments/environment';
-import { AuthGuard } from '../guards/auth.guard';
-import { HttpClientModule } from '@angular/common/http';
-import { Actor } from '../models/actor.model';
-import { getFirestore } from 'firebase/firestore';
+// import { TestBed } from '@angular/core/testing';
+// import { AuthService } from './auth.service';
+// import {provideAuth, getAuth} from '@angular/fire/auth';
+// import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
+// import { environment } from 'src/environments/environment';
+// import { AuthGuard } from '../guards/auth.guard';
+// import { HttpClientModule } from '@angular/common/http';
+// import { Actor } from '../models/actor.model';
+// import { getFirestore } from 'firebase/firestore';
 
-let testUser = new Actor()
-testUser.name = "John";
-testUser.surname = "Doe";
-testUser.email = 'newuser15@example.com',
-testUser.password = 'password123';
-testUser.role = 'CONSUMER';
-testUser.phone = '123456789';
+// let testUser = new Actor()
+// testUser.name = "John";
+// testUser.surname = "Doe";
+// testUser.email = 'newuser15@example.com',
+// testUser.password = 'password123';
+// testUser.role = 'CONSUMER';
+// testUser.phone = '123456789';
 
-function delay(ms: number) {
-  return new Promise(resolve => setTimeout(resolve, ms));
-}
+// function delay(ms: number) {
+//   return new Promise(resolve => setTimeout(resolve, ms));
+// }
 
-describe('AuthService', () => {
-  let service: AuthService;
-  let auth: any;
+// describe('AuthService', () => {
+//   let service: AuthService;
+//   let auth: any;
 
-  beforeEach(() => {
-    TestBed.configureTestingModule({
-      imports: [
-        HttpClientModule,
-        provideFirebaseApp(() => initializeApp(environment.firebaseConfig)),
-        provideAuth(() => getAuth()),
-      ],
-      providers: [AuthGuard]
-    });
-    service = TestBed.inject(AuthService);
-    auth = getAuth();
-  });
+//   beforeEach(() => {
+//     TestBed.configureTestingModule({
+//       imports: [
+//         HttpClientModule,
+//         provideFirebaseApp(() => initializeApp(environment.firebaseConfig)),
+//         provideAuth(() => getAuth()),
+//       ],
+//       providers: [AuthGuard]
+//     });
+//     service = TestBed.inject(AuthService);
+//     auth = getAuth();
+//   });
 
-  it('should be created', () => {
-    expect(service).toBeTruthy();
-  });
+//   it('should be created', () => {
+//     expect(service).toBeTruthy();
+//   });
 
-  it('should return roles', () => {
-    expect(service.getRoles()).toEqual(['EXPLORER', 'MANAGER', 'SPONSOR']);
-  });
+//   it('should return roles', () => {
+//     expect(service.getRoles()).toEqual(['EXPLORER', 'MANAGER', 'SPONSOR']);
+//   });
 
-  it('[+] should sign up successfully', async () => {
-    // Ensure the test user is removed before starting the flow
-    try {
-      await service.removeUser(testUser.email, testUser.password);
-      console.log('Test user deleted');
-    } catch (error: any) {
-      console.warn('Test user not found or already deleted:', error?.message);
-    }
-    await delay(200); // Delay after user deletion
+//   it('[+] should sign up successfully', async () => {
+//     // Ensure the test user is removed before starting the flow
+//     try {
+//       await service.removeUser(testUser.email, testUser.password);
+//       console.log('Test user deleted');
+//     } catch (error: any) {
+//       console.warn('Test user not found or already deleted:', error?.message);
+//     }
+//     await delay(200); // Delay after user deletion
 
-    // Sign up
-    const signUpResult = await service.signUp(testUser);
-    console.log('Sign up successful');
-    expect(signUpResult).toBeTruthy();
-    await delay(200); // Delay after sign-up
-  });
+//     // Sign up
+//     const signUpResult = await service.signUp(testUser);
+//     console.log('Sign up successful');
+//     expect(signUpResult).toBeTruthy();
+//     await delay(200); // Delay after sign-up
+//   });
 
-  it('[+] should log in successfully', async () => {
-    // Log in
-    const loginResult = await service.login(testUser.email, testUser.password);
-    console.log('Login successful');
-    expect(loginResult).toBeTruthy();
-  });
+//   it('[+] should log in successfully', async () => {
+//     // Log in
+//     const loginResult = await service.login(testUser.email, testUser.password);
+//     console.log('Login successful');
+//     expect(loginResult).toBeTruthy();
+//   });
 
-  it('[+] should log out successfully', async () => {
-    // Log out
-    const logoutResult = await service.logout();
-    console.log('Logout successful');
-    expect(logoutResult).toBeTruthy();
-  });
+//   it('[+] should log out successfully', async () => {
+//     // Log out
+//     const logoutResult = await service.logout();
+//     console.log('Logout successful');
+//     expect(logoutResult).toBeTruthy();
+//   });
 
-  it('[-] should fail to login with wrong credentials', async () => {
-    const badLoginData = {
-      email: 'wronguser@example.com',
-      password: 'wrongpassword'
-    };
-    try {
-      await service.login(badLoginData.email, badLoginData.password);
-      fail('Expected login to fail, but it succeeded');
-    } catch (error: any) {
-      expect(error).toBeTruthy();
-    }
-    await delay(1000); // Delay after failed login
-  });
-});
+//   it('[-] should fail to login with wrong credentials', async () => {
+//     const badLoginData = {
+//       email: 'wronguser@example.com',
+//       password: 'wrongpassword'
+//     };
+//     try {
+//       await service.login(badLoginData.email, badLoginData.password);
+//       fail('Expected login to fail, but it succeeded');
+//     } catch (error: any) {
+//       expect(error).toBeTruthy();
+//     }
+//     await delay(1000); // Delay after failed login
+//   });
+// });
